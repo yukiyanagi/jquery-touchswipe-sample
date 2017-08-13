768346247933a949f1ab3b2ae72356fd62e73581
YukiYanagi <mayonakareizi@yahoo.co.jp>
2017-08-13 18:23:00 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import jquery from 'jquery';
+const $ = jquery;
+import jqueryTouchSwipe from 'jquery-touchswipe';
+
+
+class IndexPage {
+  constructor() {
+    this.setParameter();
+    this.bindEvent();
+  }
+  setParameter() {
+    this.$target = $('html, body');
+    this.$target.swipe({figers: 2});
+  }
+  bindEvent() {
+    this.$target.swipe({
+      swipe: $.proxy(this.swipeEvent, this)  
+    });
+  }
+  swipeEvent(event, direction, distance, duration, fingerCount, fingerData) {
+    console.log("swiped :" + direction);
+  }
+}
+$(function(){
+  new IndexPage();
+});
+
